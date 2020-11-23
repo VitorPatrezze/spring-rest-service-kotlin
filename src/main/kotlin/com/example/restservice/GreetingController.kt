434d6eb7767ef30536352487c0d5a24b6baa6977
Vitor Patrezze Nunes <vitorpatrezzen@gmail.com>
2020-11-23 16:37:22 -0300

    gradle não está conseguindo montar o jar

---
+++ src/main/kotlin/com/example/restservice/GreetingController.kt
@@ -0,0 +1,19 @@
+package com.example.restservice
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class GreetingController {
+    private val counter: AtomicLong = AtomicLong()
+
+    companion object {
+        private const val template = "Hello, %s!"
+    }
+
+    @GetMapping("/greeting")
+    fun greeting(@RequestParam(value = "name", defaultValue = "World") name: String?): Greeting {
+        return Greeting(counter.incrementAndGet(), String.format(template, name))
+    }
+}
